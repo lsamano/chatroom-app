c84a48a535032a799d842ca2518e525ba82be05d
lsamano <leizl.samano@flatironschool.com>
2019-10-24 18:08:29 -0400

    refactored index.js

--- index.js
+++ index.js
@@ -2,29 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     var socket = io.connect('http://localhost:8080');
     
-    // submit text message without reload/refresh the page
+    // Makes form send messages
     const form = document.querySelector('form')
     form.addEventListener('submit', event => {
         event.preventDefault();
         socket.emit('chat_message', form.txt.value);
         form.reset()
-        return false;
     })
-    
-    // append the chat text message
-        socket.on('chat_message', function (msg) {
-            const messagesDiv = document.getElementById('messages')
-            const newLi = document.createElement('li')
-            newLi.innerHTML = msg
+
+    const appendNewMessage = message => {
+        const messagesDiv = document.getElementById('messages')
+        const newLi = document.createElement('li')
+        newLi.innerHTML = message
         messagesDiv.append(newLi);
-    });
+    }
+    
+    // appends received messages
+    socket.on('chat_message', appendNewMessage);
+
     // append text if someone is online
-        socket.on('is_online', function (username) {
-            const messagesDiv = document.getElementById('messages')
-            const newLi = document.createElement('li')
-            newLi.innerHTML = username
-            messagesDiv.append(newLi);
-    });
+    socket.on('is_online', appendNewMessage);
+
     // ask username
     var username = prompt('Please tell me your name');
     socket.emit('username', username);
